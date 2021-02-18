2a2a7141286101e7fb007ef65f47d9b872598dba
Abay <abay.reushenov@gmail.com>
2021-02-18 16:01:37 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,79 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import CoinGecko from 'coingecko-api';
+const coinGeckoClient = new CoinGecko();
 
-export default function Home() {
+export default function Home(props) {
+  const { data } = props.result;
+  console.log(data);
+  const formatPercent = (number) => `${new Number(number).toFixed(2)}%`;
+
+  const formatDollar = (number, maximumSignificantDigits) =>
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumSignificantDigits,
+    }).format(number);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Криптовалюты</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h4>Криптовалюты в реальном времени</h4>
+      <div class="table-responsive-sm">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Символ</th>
+              <th scope="col">24часа изм.</th>
+              <th scope="col">Цена</th>
+              <th scope="col">Рыночная капитализация</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((coin, index) => (
+              <tr key={coin.id}>
+                <th scope="row">{index + 1}</th>
+                <td>
+                  <img
+                    src={coin.image}
+                    style={{ width: 25, height: 25, marginRight: 10 }}
+                  />
+                  {coin.symbol.toUpperCase()}
+                </td>
+                <td>
+                  <span
+                    className={
+                      coin.price_change_percentage_24h > 0
+                        ? 'text-success'
+                        : 'text-danger'
+                    }
+                  >
+                    {formatPercent(coin.price_change_percentage_24h)}
+                  </span>
+                </td>
+                <td>{formatDollar(coin.current_price, 20)}</td>
+                <td>{formatDollar(coin.market_cap, 12)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const params = {
+    order: CoinGecko.ORDER.MARKET_CAP_DESC,
+  };
+  const result = await coinGeckoClient.coins.markets({ params });
+  return {
+    props: {
+      result,
+    },
+  };
 }
---
+++ readme.txt
@@ -0,0 +1,45 @@
+npm install express --save
+npm install -g create-next-app
+create-next-app coinmarketcap-clone
+//-------------------------------------------------------------
+получаем:
+
+Creating a new Next.js app in /home/abay/elbrus/phasa2B/week3/api-coin-market-cap/coinmarketcap-clone.
+
+Installing react, react-dom, and next using npm...
+
+
+added 293 packages, and audited 294 packages in 14s
+
+20 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+
+Initialized a git repository.
+
+Success! Created coinmarketcap-clone at /home/abay/elbrus/phasa2B/week3/api-coin-market-cap/coinmarketcap-clone
+Inside that directory, you can run several commands:
+
+  npm run dev
+    Starts the development server.
+
+  npm run build
+    Builds the app for production.
+
+  npm start
+    Runs the built app in production mode.
+
+We suggest that you begin by typing:
+
+  cd coinmarketcap-clone
+  npm run dev
+  //---------------------------------------
+
+npm i coingecko-api
+npm i bootstrap
+13:02
+
+в coinmarketcap-clone/pages/_app.js
+
+
