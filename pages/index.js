4f0e488016df9737d9a3dfe673a90c6ba4484c23
Abay <abay.reushenov@gmail.com>
2021-02-19 17:17:19 +0300

    Fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+// import { Route, BrowserRouter } from 'react-router-dom';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import CoinGecko from 'coingecko-api';
